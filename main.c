dd534298e044394d4a99d865d8a5c45403be1c07
Saif Al-Din Samy <seifowlen@proton.me>
2025-05-17 03:42:12 +0300

    those were added by mistake lol

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "window.h"
-
-int main(int argc, char **argv){
-  int status;
-  AdwApplication *app = window_init();
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  return status;
-}
--- parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-void read_node();
-char *parse_main();
-
-#endif
